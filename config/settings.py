3354bc452a9df4e5d380cea8a77b12707271bf5d
dopza86 <dopza86@gmail.com>
2020-08-09 09:05:02 +0900

    installing psycopg2

--- config/settings.py
+++ config/settings.py
@@ -108,7 +108,7 @@ else:
 
     DATABASES = {
         "default": {
-            "ENGINE": "django.db.backends.postgresql_psycopg2",
+            "ENGINE": "django.db.backends.postgresql",
             "HOST": os.environ.get("RDS_HOST"),
             "NAME": os.environ.get("RDS_NAME"),
             "USER": os.environ.get("RDS_USER"),
